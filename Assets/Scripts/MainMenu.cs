5e7d88418994d2688b80adc8cf19063d01276351
ImandaSyahrul <ImandaSyahrul@github.com>
2020-02-23 22:48:37 +0700

    Import from local repository

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	// Start is called before the first frame update
+	
+
+	public void Play()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void ExitGame()
+	{
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();	
+#endif
+	}
+}
