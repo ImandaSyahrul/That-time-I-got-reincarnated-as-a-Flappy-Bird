ed8c8e740ab798b846b6ebcccc1990d27028d916
ImandaSyahrul <ImandaSyahrul@github.com>
2020-03-03 00:25:50 +0700

    Add bullet script to develop on shooting branch

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private float speed; //kecepatan peluru
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = (transform.forward) * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //menghancurkan pipe
+        if (collision.gameObject.CompareTag("Obstacle")) 
+            Destroy(collision.gameObject);
+    }
+}
