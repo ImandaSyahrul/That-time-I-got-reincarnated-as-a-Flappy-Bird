7d759edbf0a2511d0ee559e71819e0638e0e0f93
ImandaSyahrul <ImandaSyahrul@github.com>
2020-04-18 18:59:48 +0700

    Add logic for table num 16 to 19

--- Assets/Scripts/CodeCoverage.cs
+++ Assets/Scripts/CodeCoverage.cs
@@ -18,15 +18,27 @@ public class CodeCoverage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Kebutuhan testing fitur code coverage
         if (Input.GetKeyDown(KeyCode.D))
         {
             dCounter++;
             switch (dCounter)
             {
-                case 5:Debug.Log("5 times");break;
-                case 10: Debug.Log("10 times"); break;
-                case 20: Debug.Log("20 times"); break;
+                case 5:Debug.Log("d 5 times");break;
+                case 10: Debug.Log("d 10 times"); break;
+                case 20: Debug.Log("d 20 times"); break;
                 default: Debug.Log("clicking D key");break;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.F))
+        {
+            fCounter++;
+            switch (fCounter)
+            {
+                case 5: Debug.Log("f 5 times"); break;
+                case 10: Debug.Log("f 10 times"); break;
+                case 20: Debug.Log("f 20 times"); break;
+                default: Debug.Log("clicking F key"); break;
             }
         }
         else
@@ -39,6 +51,24 @@ public class CodeCoverage : MonoBehaviour
                 case 20: Debug.Log("idle 20 times"); break;
                 default: Debug.Log("idle");break;
             }
+        }
+
+        switch (idleCounter)
+        {
+            case 5: 
+                switch (dCounter)
+                {
+                    case 5: Debug.Log("all counter count at 5");break;
+                    default: Debug.Log("waiting for sync");break;
+                }break;
+            case 10:
+                switch (dCounter)
+                {
+                    case 10: Debug.Log("all counter count at 10"); break;
+                    default: Debug.Log("waiting for sync"); break;
+                }
+                break;
+            default: break;
         }
     }
 }
